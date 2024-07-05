9c3c5305ca9be22e4dd559f798ab4e88dcae153d
Ayten <ababakishiyeva@std.beu.edu.az>
2024-07-05 12:08:20 +0300

    lesson 5hw5 added

---
+++ hw5/hw5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace hw5
+{
+    internal class Program
+    {
+
+        // Task3
+        //static void Main(string[] args)
+        //{
+        //    String name = "    hello    ";
+        //    fix(name);
+        //}
+        //public static void fix(string a) {
+        //    foreach (var i in a)
+        //    {
+        //        if(i!=' ') Console.Write(i);
+        //    }
+
+        //}
+        //task2
+        // return type
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(sum(12));
+
+
+        //}
+        //public static int sum(int n)
+        //{
+        //    int sumofeven = 0;
+        //    for (int i = 0; i <= n; i++)
+        //    {
+        //        if (i % 2 == 0) sumofeven += i;
+        //    }
+        //    return sumofeven;
+        //}
+        //void type
+        //static void Main(string[] args)
+        //{
+        //    sum(10);
+        //}
+        //public static void sum(int n)
+        //{
+        //    int sumofeven = 0;
+        //    for (int i = 0; i <= n; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //        {
+        //            sumofeven += i;
+        //        }
+
+        //    }
+        //    Console.WriteLine(sumofeven);
+
+        //}
+        //Task 1
+        static void Main(string[] args) {
+            up( "HEllo WOrd");
+        }
+        public static void up(string name) {
+            for (int i = 0; i < name.Length; i++) {
+                if (char.IsUpper(name[i]) ) Console.Write(name[i] +" ");
+            }
+        }
+
+
+    }
+}

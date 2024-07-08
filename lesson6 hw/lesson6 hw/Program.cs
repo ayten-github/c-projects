a9ea5c6baaa81c6d65595f3109ba330fb3af082f
Ayten <ababakishiyeva@std.beu.edu.az>
2024-07-08 09:45:28 +0300

    lesson7 hw added

--- lesson6 hw/lesson6 hw/Program.cs
+++ lesson6 hw/lesson6 hw/Program.cs
@@ -2,89 +2,9 @@
 {
     internal class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    string s = "salammmmaaa";
-        //    Console.WriteLine(correctpas(s)) ;
-
-        //}
-        //public static bool correctpas(string a) {
-        //    if (a.Length >= 8) {
-        //        int let = 0, dig = 0, par = 0;
-        //        foreach (var item in a)
-        //        {
-        //            bool flag = false;
-        //            if (char.IsLetterOrDigit(item) || char.IsPunctuation(item)) {
-        //                if (char.IsDigit(item))
-        //                {
-        //                    dig++;
-        //                }
-        //                else if (char.IsLetter(item))
-        //                {
-        //                    let++;
-        //                }
-        //                else par++;
-        //        }
-        //            if (dig >= 1 && let >= 1 && par >= 1) return true;
-
-        //        }
-
-        //    }
-        //    return false;
-        //}
-        //static void Main(string[] args)
-        //{
-
-        //}
-        //public static void fun(string a) {
-        //    for (int i = 0; i < a.Length; i++) { 
-
-        //    while(char.IsLetter(a[i])) {
-        //            if (a[i] == ' ') {
-        //                Console.WriteLine();
-        //            } 
-        //    }
-
-
-        //}
-        //SUBSTRInG
-
-        //static void Main(string[] args)
-        //{
-        //    string s = "salamamsdnad";
-        //    int n = 5, m = 3;
-        //    sub(s, n, m);
-        //}
-        //public static void sub(string s, int a, int b)
-        //{
-        //    for (int i = a; i < b + a; i++)
-        //    {
-        //        Console.Write(s[i]);
-
-        //    }
-
-        //}
-
-        //REPLACE
         static void Main(string[] args)
         {
-            string s = "salam";
-            char k='s';
-            char l = 'm';
-            rep (s,k, l);
-        }
-        public static void rep(string a, char b,char c) {
-            foreach (var item in a)
-            {
-                if (item == b) {
-                    b = c;
-                
-                }
-               
-                
-            }
-            Console.WriteLine(a);
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
---
+++ lesson7hw/lesson7hw/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace lesson7hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+            Rayon rayon = new Rayon();
+            Olke olke = new Olke();
+
+        }
+        public class City {
+            public string Name; 
+            public int Territory;
+            public int Numofpeople;
+            public City() { 
+
+            }
+            public City(string name, int teritory, int people) { 
+                Name = name;
+                Territory = teritory;
+                Numofpeople=people;
+
+            }
+        }
+        public class Rayon {
+            public string Name;
+            public int Territory;
+            public int Numofpeople;
+            public double Budget;
+
+            public Rayon() { 
+            
+            }
+            public Rayon(string name,int territory)
+            {
+                Name = name;
+                Territory=territory;
+            }
+            public Rayon(string name, int territory, int numofpeople, double budget) : this(name, territory)
+            {
+              
+                Numofpeople = numofpeople;
+                Budget = budget;
+            }
+
+        }
+        public class Olke {
+            public string Name;
+            public int Territory;
+            public int Numofpeople;
+            public double Budget;
+            public int Numbofarmy;
+            public int Numbofcity;
+            public Olke() { 
+            
+            }
+            public Olke(string name, int territory, int numofpeople, double budget, int numbofarmy, int numbofcity)
+            {
+                Name = name;
+                Territory = territory;
+                Numofpeople = numofpeople;
+                Budget = budget;
+                Numbofarmy = numbofarmy;
+                Numbofcity = numbofcity;
+            }
+        }
+    }
+}
